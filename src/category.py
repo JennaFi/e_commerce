6ff3dc9b0f552b14cb11bfbb87e6cb0471d7c6db
JennaFi <fi@mail.com>
2024-08-28 01:01:22 +0200

    hw 16_1

--- src/category.py
+++ src/category.py
@@ -36,6 +36,9 @@ class Category:
 
 
     def add_product(self, product: Product):
-        self.__products.append(product)
-        Category.product_count += 1
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise TypeError
 
